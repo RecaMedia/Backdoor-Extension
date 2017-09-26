1bb005298678eb6330d958111a001c476e9b5e6e
Shannon Reca <shannonreca@outlook.com>
2017-09-26 14:38:41 -0400

    Continued progress with creating package.

--- index.js
+++ index.js
@@ -1,21 +1,73 @@
 #!/usr/bin/env node --harmony
 var co = require('co');
-var prompt = require('co-prompt');
+var fs = require('fs');
+var getInstalledPath = require('get-installed-path');
+var path = require('path');
 var program = require('commander');
+var prompt = require('co-prompt');
+var sh = require("shelljs");
 
-program.arguments('<file>')
-  .option('-n, --name <name>', 'Name of extension.')
-  .option('-p, --ns <ns>', 'Namespace for extension.')
-  .option('-ns, --author <author>', 'Author of extension.')
-  .option('-u, --url <url>', 'URL to author\'s website.')
-  .option('-l, --license <url>', 'License for extension.')
-  .action(function(file) {
-  co(function *() {
-    var name = yield prompt('name: ');
-    var ns = yield prompt('namespace: ');
-    var author = yield prompt('author: ');
-    var url = yield prompt('website: ');
-    var license = yield prompt('license: ');
-    console.log('Name: %s Namespace: %s File: %s', name, ns, file);
+var cwd = sh.pwd().stdout;
+var package_dir;
+
+getInstalledPath('backdoor-extension').then((path) => {
+  package_dir = path;
+  init();
+})
+
+function copy(from, to) {
+  return new Promise(function (resolve, reject) {
+    fs.access(from, fs.F_OK, function (error) {
+      if (error) {
+        reject(error);
+        return false;
+      } else {
+        var inputStream = fs.createReadStream(from);
+        var outputStream = fs.createWriteStream(to);
+
+        function rejectCleanup(error) {
+            inputStream.destroy();
+            outputStream.end();
+            reject(error);
+            return false;
+        }
+
+        inputStream.on('error', rejectCleanup);
+        outputStream.on('error', rejectCleanup);
+
+        outputStream.on('finish', resolve);
+
+        inputStream.pipe(outputStream);
+        return true;
+      }
+    });
   });
-});
+}
+
+function init() {
+  program.arguments('<file>')
+    .option('-n, --name <name>', 'Name of extension. (e.g. My Cool Extension)')
+    .option('-p, --ns <ns>', 'Namespace for extension. (e.g. MyCoolExtension)')
+    .option('-ns, --author <author>', 'Author of extension. (e.g. John Doe)')
+    .option('-u, --url <url>', 'URL to author\'s website. (e.g. http://mydomain.com)')
+    .option('-l, --license <url>', 'License for extension. (e.g. MIT)')
+    .action(function(file) {
+    co(function *() {
+      // var name = yield prompt('name: ');
+      var ns = yield prompt('namespace: ');
+      // var author = yield prompt('author: ');
+      // var url = yield prompt('website: ');
+      // var license = yield prompt('license: ');
+      // console.log('Name: %s Namespace: %s File: %s', name, ns, file);
+      console.log("Current Dir: ",cwd);
+      console.log("Package Dir: ",package_dir);
+
+      if (copy(package_dir+'/template/test.txt', cwd+'/'+ns+'/test.txt')) {
+        console.log('Your extension\'s base has been created.');
+      }
+
+      // End script.
+      process.exit();
+    });
+  }).parse(process.argv);
+}
