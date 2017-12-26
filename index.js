ff2bb372d063c5a7ee65cdddebd9e48de4dd72ed
Shannon Reca <shannonreca@outlook.com>
2017-12-26 15:01:59 -0500

    Missed rename arg.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var sh = require("shelljs");
 var cwd = sh.pwd().stdout;
 var package_dir;
 
-getInstalledPath('backdoor-extension').then((path) => {
+getInstalledPath('bkdr-ext').then((path) => {
   package_dir = path;
   init();
 })
